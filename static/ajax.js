a0444d105738613e658b4f116674072bbf0ca4b3
RCElucidator7 <G00332826@gmit.ie>
2017-11-24 20:47:34 +0000

    Gave Frank a thumbnail beside his responses

--- static/ajax.js
+++ static/ajax.js
@@ -17,7 +17,7 @@ form.keypress(function(event){
     const prompt = {"input" : userInput }
     $.get("/chatbot", prompt)
         .done(response => {
-            const output = "<li class='list-group-item  text-right list-group-item-warning'>" + response + " : Frank! " + "</li>";
+            const output = "<li class='list-group-item  text-right list-group-item-warning'>" + response + " : Frank! <img src='/assets/frank_id.png' height='50' width='50'>" + "</li>";
             $("html, body").scrollTop($("body").height());
             setTimeout(function(){list.append(output)}, 2000);
         }).fail(() => {
